17fb0de9f92a1d03516a86f24893f581789f82aa
ngocl <ngoc.lehong3012@gmail.com>
2021-03-29 14:04:56 +0700

    Bài 9

---
+++ class/lab9/script.js
@@ -0,0 +1,23 @@
+let a = Number(prompt("Nhập số a"));
+let b = Number(prompt("Nhập số b"));
+
+function sum(a, b) {
+    return a+b;
+
+
+function sub(a, b) {
+    return a-b;
+}
+
+function mul(a, b) {
+    return a*c;
+}
+
+function div(a, b) {
+    return a/b;
+}
+
+console.log(a + " + " + b " = " + sum(a, b));
+console.log(a + " - " + b " = " + sub(a, b));
+console.log(a + " * " + b " = " + mul(a, b));
+console.log(a + " / " + b " = " + div(a, b));

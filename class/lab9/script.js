7b5b358320c1e8f9cc9bd39086c54737eef36b47
Khun <ngoc.lehong301@gmail.com>
2021-03-30 21:36:34 +0700

    bài tập buổi 9

--- class/lab9/script.js
+++ class/lab9/script.js
@@ -1,23 +1,70 @@
-let a = Number(prompt("Nhập số a"));
-let b = Number(prompt("Nhập số b"));
+// Bài 1
+// let a = Number(prompt("Nhập bán kính hình tròn"));
 
-function sum(a, b) {
-    return a+b;
+// function chuVi(a) {
+//     return 2 * a * 3.14;
+// }
 
+// function dienTich(a) {
+//     return a * a * 3.14;
+// }
 
-function sub(a, b) {
-    return a-b;
-}
+// console.log("Chu vi hình tròn = " + chuVi(a));
+// console.log("Diện tích hình tròn = " + dienTich(a));
 
-function mul(a, b) {
-    return a*c;
-}
+// Bài 2
+// let a = Number(prompt("Nhập giá trị cm"));
 
-function div(a, b) {
-    return a/b;
-}
+// function mm(a) {
+//   return a * 10;
+// }
 
-console.log(a + " + " + b " = " + sum(a, b));
-console.log(a + " - " + b " = " + sub(a, b));
-console.log(a + " * " + b " = " + mul(a, b));
-console.log(a + " / " + b " = " + div(a, b));
+// function m(a) {
+//   return a / 100;
+// }
+
+// function km(a) {
+//   return a / 100000;
+// }
+
+// console.log("Giá trị quy đổi sang mm = " + mm(a) + " mm");
+// console.log("Giá trị quy đổi sang m = " + m(a) + " m");
+// console.log("Giá trị quy đổi sang km = " + km(a) + " km");
+
+// Bài 3
+// let a = Number(prompt("Nhập giá trị nhiệt độ Celsius"));
+
+// function fahrenheit(a) {
+//     return (a * 1.8) + 32;
+// }
+// function kevin(a) {
+//     return a + 273.15;
+// }
+
+// console.log("Giá trị nhiệt độ F = " + fahrenheit(a) + " F");
+// console.log("Giá trị nhiệt độ K = " + kevin(a) + " K");
+
+// Bài 4
+// let a = Number(prompt("Nhập số giây"));
+
+// function hour(a) {
+//   return a / 3600;
+// }
+
+// function minute(a) {
+//   return a / 60;
+// }
+
+// function second(a) {
+//   return a;
+// }
+
+// console.log(
+//   "Số giờ/phút/giây tương ứng = " +
+//     hour(a) +
+//     "h: " +
+//     minute(a) +
+//     "m: " +
+//     second(a) +
+//     "s"
+// );

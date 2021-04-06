6d55cce97a7a0de063de3ec961a59f219ad0ad0f
Khun <ngoc.lehong301@gmail.com>
2021-04-07 00:46:48 +0700

    bài tập buổi 11

--- hw/bai 11/script.js
+++ hw/bai 11/script.js
@@ -13,6 +13,17 @@
 // }
 
 // VCT tính và in ra tổng bội chung của 3 và 5 trong khoảng 0 -> 100
+// let sum = 0;
+
+// for (i = 0; i <= 100; i++) {
+//   if ((i % 3 == 0) && (i % 5 == 0)) {
+//     sum += i;
+//   } 
+// }
+
+// console.log(sum);
+
+
 
 // VCT nhập vào một số n, kiểm tra số đó có phải số nguyên tố hay không và in ra kết quả
 // let n = Number(prompt("Nhập số n:"));
@@ -39,17 +50,15 @@
 // if (a >= b) {
 //     console.log("Không hợp lệ");
 // } else {
-//     for (i = a; i <= b; i++) {
-//         if (i < 2) {
-//             continue;
-//         } else for (j = 2; j <= Math.sqrt(i); j++) {
-//             if (j % i == 0) {
-//                 continue;
-//             } else {
-//                 console.log(j);
-//             }
-//         }
+//   for (let i = a; i <= b; i++) {
+//     for (let j = 2; j <= i; j++) {
+//       if (i % j == 0) {
+//         break;
+//       } else {
+//         console.log(i);
+//       }
 //     }
+//   }
 // }
 
 // VCT in ra bảng cửu chương ngược (từ 10 -> 1)
@@ -70,25 +79,96 @@
 // VCT in ra chữ số đầu và cuối của một số. VD 12345 -> 15
 
 // VCT kiểm tra và in ra một số có phải số Palindrome hay không
+// let num = Number(prompt("Nhập một số:"));
+// let reverse = 0;
+// let temp = num;
 
-let n = Number(prompt("Nhập số bất kỳ:"));
-let sum = 0;
+// while (num > 0) {
+//   rem = num % 10;
+//   num = parseInt(num / 10);
+//   reverse = reverse * 10 + rem;
+// }
 
-for (i = n; n != 0; n = n / 10) {
-  r = n % 10;
-  sum = sum * 10 + r;
-}
+// if (reverse == temp) {
+//   console.log(temp + " là số Plindrome");
+// } else {
+//   console.log(temp + " KHÔNG phải là số Plindrome");
+// }
 
-if (i == sum) {
-  console.log(n + " là số Palindrome");
-} else {
-  console.log(n + " KHÔNG phải là số Palindrome");
-}
+
 
 // VCT kiểm tra và in ra một số có phải số Armstrong hay không
+// let num = prompt("Nhập một số bất kỳ:");
+// let temp = num;
+// let sum = 0;
+
+// while (temp > 0) {
+//   // 153/10 = 3
+//   let digit = temp % 10;
+//   sum += digit ** 3;
+//   temp = parseInt(temp / 10);
+// }
+// if (sum == num) {
+//   console.log(num + " là số Amstrong");
+// } else {
+//   console.log(num + " không phải là số Amstrong")
+// }
+
+
 
 // VCT tính và in ra giai thừa Factorial của một số
+// let num = prompt("Nhập số bất kỳ:");
+// let fact = 1;
+// if (num == 0) {
+//   console.log("Giai thừa của " + num + " là 1");
+// } else if (num < 0) {
+//   console.log("Không có giai thừa hợp lệ");
+// } else {
+//   for (let i = 1; i <= num; i++){
+//     fact = fact * i;
+//   }
+//   console.log(" Giai thừa của " + num + " là " + fact);
+// }
+
+
+
 
 // VCT kiểm tra và in ra một số có phải số Perfect hay không
+// let num = Number(prompt("Nhập số bất kỳ"));
+// let s = 0;
+// let temp = num;
+
+// for (let i = 1; i <= parseInt(num/2); i++) {
+//   if (num % i == 0) {
+//     s = s + i;
+//   }
+// }
+
+// if (s == num) {
+//   console.log(num + " là số Perfect");
+// } else {
+//   console.log(num + " KHÔNG phải là số Perfect");
+// }
+
+
+
 
 // VCT kiểm tra và in ra một số có phải số Strong hay không
+// let num = Number(prompt("Nhập số bất kỳ:"));
+// let originalNum = num;
+// let sum = 0;
+
+// while (num > 0) {
+//   fact = 1;
+//   lastdigit = num % 10;
+//   for (i = 1; i <= lastdigit; i++){
+//     fact = fact * i;
+//   }
+//   sum = sum + fact;
+//   num = parseInt(num / 10);
+// }
+// if (originalNum == sum) {
+//   console.log(originalNum + " là số Strong");
+// } else {
+//   console.log(originalNum + " KHÔNG phải là số Strong");
+// }
